4e67617b7aa6b8cdfafc5999f75c835d32e973b4
xdKya <90745676+xdKya@users.noreply.github.com>
2023-08-08 19:08:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
 
-var rodrigo,rodrigo_run;
+var trex,trex_img;
 var bordas;
+var chao;
 
 function preload(){
   //pré carrega as imagens do jogo
-  rodrigo_run = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_img = loadAnimation("trex3.png","trex4.png");
 
 }
 
 function setup(){
   createCanvas(600,200);
 
-  rodrigo = createSprite(50,100,20,20);
-  rodrigo.addAnimation("running",rodrigo_run);
-  rodrigo.scale = 0.5;
+  trex = createSprite(50,100,20,20);
+  trex.addAnimation("running",trex_img);
+  trex.scale = 0.5;
 
   bordas = createEdgeSprites();
 }
@@ -22,13 +23,13 @@ function draw(){
   background('white');
 
   if(keyDown("space")){
-    rodrigo.velocityY = -10;
+    trex.velocityY = -10;
   }
 
   //gravidade
-  rodrigo.velocityY = rodrigo.velocityY + 0.9;
+  trex.velocityY = trex.velocityY + 0.9;
 
-  rodrigo.collide(bordas);
+  trex.collide(bordas);
 
   drawSprites();
 }
